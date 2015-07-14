4edd97a340f9eb060ee5b07a0cd02ad4ec5a5cb2
MKS-students <mks.students@hackreactor.com>
2015-07-14 11:19:11 -0500

    Minor styling changes. Real-time multiplayer userinput

--- client/games/game.js
+++ client/games/game.js
@@ -1,9 +1,23 @@
 angular.module("app").controller("GameController", ['$scope','$stateParams', '$meteor', '$window',
   function($scope, $stateParams, $meteor, $window){
-    $scope.id = $stateParams.gameId;
     $scope.match = $scope.$meteorObject(Games, {_id: $stateParams.gameId });
-    $scope.creator = $scope.match.creator.email;
+    // $scope.userInput  = $scope.match.players
+    $scope.id = $stateParams.gameId;
+
+    $scope.currentPlayerIndex = function(){
+      var currentUserId = Meteor.userId();
+      for(var i = 0; i < $scope.match.players.length; i++){
+        if($scope.match.players[i].id === currentUserId)
+          return i;
+      }
+    }
+
+    // var currentPlayerIndex = currentPlayerInput();
+    // $scope.userInput = $scope.match.players[currentPlayerIndex].input;
+
+    // $scope.userInput =
+
     // $scope.currentPlayer = $scope.$meteorObject(Meteor.users, {_id: Meteor.userId()});
     // Need to attach a scope variable to the users input in the thing
-
+    // console.log(currentPlayerIndex);
 }]);
