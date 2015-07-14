eb73dada67637614be76457317354c69d4d31199
MKS-students <voxlol@outlook.com>
2015-07-13 19:49:48 -0500

    angular hooked up and working

--- client/maincontroller.js
+++ client/maincontroller.js
@@ -1,4 +1,18 @@
-angular.module("app").controller("MainController", ['$scope', '$stateParams',
-  function($scope, $stateParams){
-    $scope.greeting = "HELLO";
+angular.module("app").controller("MainController", ['$scope', '$stateParams', '$meteor',
+  function($scope, $stateParams, $meteor){
+    $scope.greeting = $scope.$meteorObject(Toy , {_id: "mE7aP6RpQrXWBKeSu"});
+    $scope.log = function(){
+      console.log('change!');
+      if($scope.greeting.booga)
+        $scope.greeting.booga+= 10;
+      else
+        $scope.greeting.booga = 1;
+    }
+    $scope.checkLength = function(length){
+      if(length >=  10)
+        return true
+      else
+        return false;
+    }
+    console.log($scope.greeting);
 }]);
